1b307b5023f462ad595052fd8f0d5fcbba005fd2
Helen Smith <5b64nnhi@students.codeinstitute.net>
2024-10-21 17:28:23 +0000

    Feat: Add code to generate randon color code

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# import random library
+import random
+#define colors available to choose from, code length and how many attempts
+colors = ["Red", "Blue", "Green", "Yellow", "Black", "Orange"]
+code_length = 4
+max_attempts = 10
+#generate randon code
+code = random.choices(colors, k=code_length)
+
+print(code)
