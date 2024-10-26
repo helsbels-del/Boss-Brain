abbbd2bb6a6464ff8443818e0a3acf0d962cd363
Helen Smith <5b64nnhi@students.codeinstitute.net>
2024-10-26 10:59:16 +0000

    feat: Import os and add clear scrren function

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 # import random library
 import random
+import os
 # Constant variables - colors available to choose from, code length and how many attempts
 colors = ["Red", "Blue", "Green", "Yellow", "Black", "Orange"]
 code_length = 4
@@ -19,7 +20,7 @@ def main_page():
         try:
             menu_choice = int(input(" Press choice: \n"))
             if menu_choice == 1:
-                run_game()
+                play_game()
                 break
             elif menu_choice == 2:
                 instructions()
@@ -65,29 +66,38 @@ def play_game():
     print(f"Code Length: {code_length}, Max Attempts: {max_attempts}")
 
 # while loop to itereate over game until max attempts made
-attempts = 0
+    attempts = 0
+    code = generate_random_code()
+    print(code)
+    while attempts < max_attempts:
+        answer = input(f"Attempt {attempts + 1}/{max_attempts}. Enter your answer: ").strip().split()
+        if len(answer) != code_length or not all(color in colors for color in answer):
+            print("Incorrect Answer! Try again!")
+            continue
 
-while attempts < max_attempts:
-    answer = input(f"Attempt {attempts + 1}/{max_attempts}. Enter your answer: ").strip().split()
-    if len(answer) != code_length or not all(color in colors for color in answer):
-        print("Incorrect Answer! Try again!")
-        continue
+        correct_position = sum(a == c for a, c in zip(answer, code))
+        correct_color = sum(min(answer.count(c), code.count(c)) for c in set(code))
+        correct_color -= correct_position
 
-    correct_position = sum(a == c for a, c in zip(answer, code))
-    correct_color = sum(min(answer.count(c), code.count(c)) for c in set(code))
-    correct_color -= correct_position
+        print(f"{correct_position} colors in the correct place!")
+        print(f"{correct_color} correct colors but in the wrong place!")
 
-    print(f"{correct_position} colors in the correct place!")
-    print(f"{correct_color} correct colors but in the wrong place!")
+        if correct_position == code_length:
+            print("Congratulations! You cracked the code. You have a Boss Brain!")
+            exit()
 
-    if correct_position == code_length:
-        print("Congratulations! You cracked the code. You have a Boss Brain!")
-        exit()
+        attempts += 1
 
-    attempts += 1
-
-print("You ran out of attempts!")
-
-
+    print("You ran out of attempts!")
+# clear screen function
+def clear_screen():
+    """
+    Clears the terminal screen/window
+    """
+    if os.name == "posix":
+        os.system("clear")
+    else:
+        os.system("cls")
+main_page()
 
 
