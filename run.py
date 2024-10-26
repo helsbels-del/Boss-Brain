1917d14708ee247f4bf08caebd8c92538abcf9a5
Helen Smith <5b64nnhi@students.codeinstitute.net>
2024-10-26 13:53:32 +0000

    Fix: correct colors for color choices and alter background color, though I haven't done this correctly for now so need to come back to this.

--- run.py
+++ run.py
@@ -45,8 +45,8 @@ def instructions():
     clear_screen()
                 
     print("The Boss Brain Challenge will create a 4 color code for you to decipher.\n")
-    print("Your choice of colors is: \033[1;31;47m Red \033[1;34;47m Blue \033[1;32;47m Green \033[1;33;47m Yellow \033[1;37;47m White \033[1;35;47m Pink \n" )
-    print("\033[1;37;40m Find the code in 8 or less attempts to become a Boss Brain!") 
+    print("Your choice of colors is: \033[1;31:47m Red \033[1;34:47m Blue \033[1;32:47m Green \033[1;33:47m Yellow \033[1;37:47m White \033[1;35:47m Pink \n" )
+    print("\033[1;37:40m Find the code in 8 or less attempts to become a Boss Brain!") 
 
 
 # generate random code
